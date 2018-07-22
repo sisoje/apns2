41e91d9fc9e4e7c270233b9251fdf27ac50df7e4
Lazar Otasevic <lazar.otasevic@1und1.de>
2018-07-22 12:00:01 +0200

    refactored code to do what it is told to do and not playing smart. now suitable for sending batches of messages removed boilerplate code and extra dependencies (tarn & bluebird)

---
+++ test/main.js
@@ -0,0 +1,40 @@
+const { APNS } = require('../lib/apns')
+
+const keyText = '-----BEGIN PRIVATE KEY-----\nBLA.BLA.BLA\n-----END PRIVATE KEY-----'
+const keyId = 'BLA'
+const teamId = 'Q28A9MLQ38'
+
+
+let deviceToken = '722cd307306e368e30d1823fa0d55d0c0bc0ed8c90eaf480633b2a62816352bb'
+let topic = 'com.redhotbits.goldprice-lite'
+
+let client = new APNS ({
+    team: teamId,
+    keyId: keyId,
+    key: keyText
+})
+
+let headers = {
+    'apns-priority': 10,
+    'apns-topic': topic
+}
+
+let payload = {
+        aps: {
+            alert: 'Testing node',
+            badge: 1,
+            sound: 'default'
+        }
+}
+
+let tokens = [deviceToken, '1']
+
+client.connect().then(() => {
+    client.send({
+        deviceTokens: tokens,
+        payload: payload,
+        headers: headers
+    }).then(results => console.log('done', results)).catch(err => console.log('can not push', err))
+}).catch(err => {
+    console.log('can not connect', err)
+})
